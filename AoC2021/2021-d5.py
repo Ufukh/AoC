ce0c280523874c4b8cd3ca86abaa97bba98230a6
Halisdemir Ufuk <ufuk.halisdemir@gmail.com>
2021-12-13 23:15:15 +0100

    day5 p1

--- AoC2021/2021-d5.py
+++ AoC2021/2021-d5.py
@@ -1,12 +1,25 @@
 import numpy as np
 import re
+from collections import Counter
 
 with open("inputd5.txt") as file:
     puzzle = file.readlines()
     mymap = np.zeros(shape=(1, 1), dtype=int)
-    line = []
+    points = []
+    points += [range(0, 5)]
+    points += range(6, 10)
+    print(points)
     for coordinates in puzzle:
-        line.append(re.findall(r"\d+", coordinates))
-    myarray = np.array(line, dtype=np.int64)
-    for i in range(0, myarray.shape[1]):
-        print(np.max(myarray[:, i]))
+        coord = re.findall(r"\d+", coordinates)
+        x1 = int(coord[0])
+        y1 = int(coord[1])
+        x2 = int(coord[2])
+        y2 = int(coord[3])
+        if x1 == x2 or y1 == y2:
+            points += [
+                (x, y)
+                for x in range(min(x1, x2), max(x1, x2) + 1)
+                for y in range(min(y1, y2), max(y1, y2) + 1)
+            ]
+    p1 = [pt for pt in Counter(points).values() if pt > 1]
+    print(len(p1))
